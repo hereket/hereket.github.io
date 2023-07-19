290e83e84c79b02475559e81f98df70d5827d8e4
tootac <alfred.salahov@gmail.com>
2023-07-20 04:35:51 +0500

    android native post

---
+++ files/handmade_android_native/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hereket.handmade_native_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import android.view.ViewGroup.LayoutParams;
+import android.view.Gravity;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        // textView = (TextView) findViewById(R.id.text_container);
+        //
+        // int value = getData();
+        // setValue(value);
+    }
+}
